53661bfb453caeabd8ca6eccc7c121ed8905fd80
Samuel Waggoner <samuel.waggoner@gmail.com>
2013-07-04 01:07:36 -0500

    InputListener 1 at a time.

--- pixl/Frame.java
+++ pixl/Frame.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Frame extends java.awt.Frame implements Runnable, WindowListener {
-  private final Screen screen = new Screen();
-  private final List<Renderable> renderables = new ArrayList<Renderable>();
-  private InputListener input;
-  
+  protected final Screen screen = new Screen();
+  protected final List<Renderable> renderables = new ArrayList<Renderable>();
+  protected InputListener input;
+
   public Frame(String title) {
     this.add(screen);
     this.addWindowListener(this);
@@ -20,31 +20,35 @@ public class Frame extends java.awt.Frame implements Runnable, WindowListener {
     this.setLocationRelativeTo(null);
     screen.requestFocus();
   }
-  
+
   public void addRenderable(Renderable renderable) {
     renderables.add(renderable);
   }
-  
-  public void addInputHandler(InputListener input) {
+
+  public void setInputListener(InputListener input) {
+    screen.removeKeyListener(this.input);
+    screen.removeMouseListener(this.input);
+    screen.removeMouseMotionListener(this.input);
+    this.input = input;
     screen.addKeyListener(input);
     screen.addMouseListener(input);
     screen.addMouseMotionListener(input);
   }
-  
-  public InputListener getInputHandler() {
+
+  public InputListener getInputListener() {
     return input;
   }
-  
+
   public void render() {
     for (Renderable r : renderables) {
       r.render(screen);
     }
     screen.render();
   }
-  
+
   @Override
   public void run() {
-    
+
     while (true) {
       render();
       try {
@@ -54,36 +58,36 @@ public class Frame extends java.awt.Frame implements Runnable, WindowListener {
       }
     }
   }
-  
+
   public void start() {
     new Thread(this).start();
   }
-  
+
   @Override
   public void windowActivated(WindowEvent arg0) {
   }
-  
+
   @Override
   public void windowClosed(WindowEvent arg0) {
   }
-  
+
   @Override
   public void windowClosing(WindowEvent arg0) {
     System.exit(0);
   }
-  
+
   @Override
   public void windowDeactivated(WindowEvent arg0) {
   }
-  
+
   @Override
   public void windowDeiconified(WindowEvent arg0) {
   }
-  
+
   @Override
   public void windowIconified(WindowEvent arg0) {
   }
-  
+
   @Override
   public void windowOpened(WindowEvent arg0) {
   }
