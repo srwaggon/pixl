7a6e1589def31d9e14ef9a0c68832b11abeab680
Samuel Waggoner <samuel.waggoner@gmail.com>
2014-01-25 13:07:37 -0600

    Refactor: extract spritesheet->canvas logic into smaller functions

--- pixl/Screen.java
+++ pixl/Screen.java
@@ -13,7 +13,6 @@ public class Screen extends Canvas {
   final int WIDTH = 200;
   final int HEIGHT = WIDTH * 10 / 16;
   final int SCALE = 3;
-  protected int xOffset = 0, yOffset = 0;
   final int PUREBLACK = -16777216;
 
   private final BufferedImage screen = new BufferedImage(WIDTH, HEIGHT,
@@ -39,31 +38,62 @@ public class Screen extends Canvas {
     bs.show();
   }
 
-  public void render(int spriteID, int x, int y) {
-    x -= xOffset;
-    y -= yOffset;
+  public void render(int spriteID, int xOffset, int yOffset) {
+    renderEachRow(spriteID, xOffset, yOffset);
+  }
 
+  private void renderEachRow(int spriteID, int xOffset, int yOffset) {
+    for (int y = 0; y < sheet.TILESIZE; y++) {
+      if (isValidSpriteSheetY(y, yOffset)) {
+        renderRow(spriteID, xOffset, y, yOffset);
+      }
+    }
+  }
+
+  private void renderRow(int spriteID, int xOffset, int y, int yOffset) {
+    for (int x = 0; x < sheet.TILESIZE; x++) {
+      if (isValidSpriteSheetX(x, xOffset)) {
+        copyPixelFromSpriteSheetToCanvas(spriteID, x, xOffset, y, yOffset);
+      }
+    }
+  }
+
+  private void copyPixelFromSpriteSheetToCanvas(int spriteID, int x,
+      int xOffset, int y, int yOffset) {
+    int sheetIndex = calculateSpriteSheetPixelIndex(spriteID, y, x);
+    int canvasIndex = calculateCanvasPixelIndex(xOffset, yOffset, y, x);
+    
+    replaceCanvasPixelIfNotBlack(sheetIndex, canvasIndex);
+  }
+
+  private boolean isValidSpriteSheetY(int y, int yOffset) {
+    return yOffset + y >= 0 && yOffset + y < HEIGHT;
+  }
+
+  private boolean isValidSpriteSheetX(int x, int xOffset) {
+    return x + xOffset >= 0 && xOffset + x < WIDTH;
+  }
+
+  private int calculateSpriteSheetPixelIndex(int spriteID, int y, int x) {
+    return x + y * sheet.width + getSpriteOffset(spriteID);
+  }
+
+  private int calculateCanvasPixelIndex(int xOffset, int yOffset, int y, int x) {
+    return xOffset + x + (yOffset + y) * WIDTH;
+  }
+
+  private void replaceCanvasPixelIfNotBlack(int sheetIndex, int canvasIndex) {
+    int colour = sheet.pixels[sheetIndex];
+    if (colour != PUREBLACK) {
+      pixels[canvasIndex] = colour;
+    }
+  }
+
+  private int getSpriteOffset(int spriteID) {
     int spriteX = spriteID % sheet.PER_ROW;
     int spriteY = spriteID / sheet.PER_ROW;
     int spriteOffset = spriteX * sheet.TILESIZE + spriteY * sheet.TILESIZE
         * sheet.width;
-
-    for (int row = 0; row < sheet.TILESIZE; row++) {
-      if (y + row >= 0 && y + row < HEIGHT) {
-
-        for (int col = 0; col < sheet.TILESIZE; col++) {
-          if (x + col >= 0 && x + col < WIDTH) {
-
-            int sheetIndex = col + row * sheet.width + spriteOffset;
-            int canvasIndex = x + col + (y + row) * WIDTH;
-            int colour = sheet.pixels[sheetIndex];
-
-            if (colour != PUREBLACK) {
-              pixels[canvasIndex] = colour;
-            }
-          }
-        }
-      }
-    }
+    return spriteOffset;
   }
 }
